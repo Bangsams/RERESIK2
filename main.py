fd8b6a7d617ecce17c9d7de8da0efb68c96d78b6
Bangsams <bangsams@example.com>
2025-08-09 18:36:49 +0700

    update

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ LANGSMITH_API_KEY = os.getenv('LANGCHAIN_API_KEY')
 if LANGSMITH_API_KEY:
     os.environ["LANGCHAIN_TRACING_V2"] = "true"
     os.environ["LANGCHAIN_API_KEY"] = LANGSMITH_API_KEY
-    os.environ["LANGCHAIN_PROJECT"] = "RERESIK-AI-Trace"
+    os.environ["LANGCHAIN_PROJECT"] = "RERESIK AI"
     ls_client = LangSmithClient(api_key=LANGSMITH_API_KEY)
 
     # Model LangChain untuk logging
